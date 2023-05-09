8f9cf4a8f02b3f5fa7cd45d6c70c84ba2dec50c1
Y2IN <dpdls0228@daum.net>
2023-05-09 17:06:31 +0900

    20230509

---
+++ 1021.py
@@ -0,0 +1,28 @@
+from collections import deque
+import sys
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+num_list = list(map(int, input().split()))
+dq = deque([i for i in range(1, n+1)])
+
+# print(dq)
+count = 0
+for num in num_list:
+	while True:
+		if dq[0] == num:
+			dq.popleft()
+			break
+		else:
+			if dq.index(num) <= len(dq) // 2:
+				print(len(dq)//2)
+				print("index" , dq.index(num), dq)
+				dq.rotate(-1)
+				count+=1
+			else:
+				print("index" , dq.index(num), dq)
+				dq.rotate(1)
+				count+=1
+print(count)
+
+
